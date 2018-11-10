6e9fa92a94019ebd38fb2e048c7792ef7af4d3e8
jistjoalal <jistjoalal@gmail.com>
2018-11-09 20:50:52 -0800

    ref attribute

--- src/App.js
+++ src/App.js
@@ -127,17 +127,27 @@ class App extends Component {
 }
 
 // a search bar
-const Search = ({ value, onChange, onSubmit, children }) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) { this.input.focus(); }
+  }
+  render() {
+    const { value, onChange, onSubmit, children } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={el => this.input = el}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 // table formatted for results from HN API
 const Table = ({ list, onDismiss }) => 
