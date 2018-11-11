4fb519e83d28cfb1b1031a31ce9e7ed4f2c525bb
jistjoalal <jistjoalal@gmail.com>
2018-11-10 20:52:40 -0800

    project reorganization

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles/index.css';
+import App from './components/App';
 
 ReactDOM.render(
-    <App style={{color: 'red'}} />,
+    <App />,
     document.getElementById('root')
 );
 
 if (module.hot) {
     module.hot.accept();
 }
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
